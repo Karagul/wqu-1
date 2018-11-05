ef066120309fc27f5f6d9ff5090e423c9442e41d
longkmt <long_kmt@live.com>
2018-11-04 23:40:54 -0500

    complete dw 1

--- WQ_DS/dw.py
+++ WQ_DS/dw.py
@@ -10,14 +10,14 @@ scripts =   pd.DataFrame(scripts_data)
 print (scripts)
 
 
-with gzip.open('./dw-data/practices.csv.gz', 'rb') as f:
-    practice_data = pd.read_csv(f)
+with gzip.open('./dw-data/practices.csv.gz', 'rb') as p:
+    practice_data = pd.read_csv(p)
     
 practices = pd.DataFrame(practice_data)
 print (practices)
 
-with gzip.open('./dw-data/chem.csv.gz', 'rb') as f:
-    chem_data = pd.read_csv(f)
+with gzip.open('./dw-data/chem.csv.gz', 'rb') as c:
+    chem_data = pd.read_csv(c)
 
 chem = pd.DataFrame(chem_data) 
 
@@ -26,4 +26,36 @@ print (chem)
 
 #Question 1: summary_statistic
 
-print (scripts.describe(percentiles, include, exclude))
+
+print ("Describe scripts: \n", scripts.describe())
+
+def summary_stats():
+    results = []
+    
+    temp = scripts.describe()
+    
+    print ("columns: " + temp.columns)
+    
+    for key in temp.columns:
+        #print ("column name: " ,key, "| row index: ", temp[key].index)
+        total = scripts[key].sum()
+        avg = float(total) / temp[key]['count']
+        std = temp[key]['std']
+        q25 = temp[key]['25%']
+        med = temp[key]['50%']
+        q75 = temp[key]['75%']
+        
+        t = (total, avg, std , q25, med, q75)
+        print ((key,t))
+        results.append((key, t))
+    
+    return results    
+    
+summary_stats()
+
+
+# Question 2: Most common item
+def most_common_item():
+    print ("group", scripts.groupby('bnf_name'))
+
+most_common_item()
