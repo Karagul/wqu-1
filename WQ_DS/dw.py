1e3a9a4e4d0d8c11cd1bb8320380b6e500400ce8
longkmt <long_kmt@live.com>
2018-11-06 00:14:06 -0500

    complete dw exercise 2

--- WQ_DS/dw.py
+++ WQ_DS/dw.py
@@ -7,21 +7,21 @@ with gzip.open('./dw-data/201701scripts_sample.csv.gz', 'rb') as f:
 
 scripts =   pd.DataFrame(scripts_data)  
 
-print (scripts)
+print (scripts.head())
 
 
 with gzip.open('./dw-data/practices.csv.gz', 'rb') as p:
     practice_data = pd.read_csv(p)
     
 practices = pd.DataFrame(practice_data)
-print (practices)
+print (practices.head())
 
 with gzip.open('./dw-data/chem.csv.gz', 'rb') as c:
     chem_data = pd.read_csv(c)
 
 chem = pd.DataFrame(chem_data) 
 
-print (chem)   
+print (chem.head())   
 
 
 #Question 1: summary_statistic
@@ -46,7 +46,7 @@ def summary_stats():
         q75 = temp[key]['75%']
         
         t = (total, avg, std , q25, med, q75)
-        print ((key,t))
+        print ("Scripts stats: ", (key,t))
         results.append((key, t))
     
     return results    
@@ -56,6 +56,16 @@ summary_stats()
 
 # Question 2: Most common item
 def most_common_item():
-    print ("group", scripts.groupby('bnf_name'))
+    
+    temp = scripts.groupby('bnf_name')['items'].sum() #group by bnf_name then take sum of items in each group
+    print ("Max bnf_name/items: ", temp.idxmax(), temp.max()) #get the index of the row with max items
+    return [(temp.idxmax(), temp.max())]
 
 most_common_item()
+
+# Question 3: Items by region
+def items_by_region():
+    joined = pd.merge(scripts,practices, how='inner', left_on = 'practice', right_on = 'A81001')
+    print (joined)
+    
+items_by_region()
